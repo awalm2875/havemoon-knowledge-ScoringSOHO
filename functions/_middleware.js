1b86945c45e0abd73bdf1e10a3c91a75c6d54e69
awalm2875 <awalm2875@users.noreply.github.com>
2026-08-12 15:40:27 +0800

    Redirect www traffic to canonical domain

---
+++ functions/_middleware.js
@@ -0,0 +1,14 @@
+const CANONICAL_HOST = "sohotation.xyz";
+
+export function onRequest(context) {
+	const url = new URL(context.request.url);
+
+	if (url.hostname === `www.${CANONICAL_HOST}`) {
+		url.hostname = CANONICAL_HOST;
+		url.protocol = "https:";
+
+		return Response.redirect(url.toString(), 301);
+	}
+
+	return context.next();
+}
